1facb4eaa832dc08696fe8e8d77bcfd6baf85185
betonit007 <timnagorski@hotmail.com>
2019-01-27 15:19:42 -0500

    Updates

---
+++ precache-manifest.4371928324691a45a071faae72999342.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f80cb5455151c35142e",
+    "url": "/fort-click/static/js/runtime~main.4f80cb54.js"
+  },
+  {
+    "revision": "527c51f9f03e326112e7",
+    "url": "/fort-click/static/js/main.527c51f9.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/fort-click/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "527c51f9f03e326112e7",
+    "url": "/fort-click/static/css/main.0b90ebe6.chunk.css"
+  },
+  {
+    "revision": "2dfeac973be70edddb90481f3937fc20",
+    "url": "/fort-click/index.html"
+  }
+];
