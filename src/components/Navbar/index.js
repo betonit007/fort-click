6d335c0df41f7eaa1c84a98a03c9fdca394f009d
betonit007 <timnagorski@hotmail.com>
2019-04-25 13:18:48 -0400

    added 4 break points to assist css grid

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 function Navbar(props) {
   return (
     <nav className="navbar">
-      <img alt="Fortnite Logo" src={props.logo}></img>
+      <img alt="Fortnite Logo" className="banner" src={props.logo}></img>
       <div className='scores'>
         <div id="score">Your Score: {props.score}</div>
         <p onClick={() => props.restartGame()} id="restart">Restart Game</p>
